28c546310380757a897a09b5d34b6c23d7ffedc6
SamoySamoy <antalois23@gmail.com>
2022-10-08 09:29:39 +0700

    bomb

--- src/uet/oop/bomberman/entities/blocks/Bomb.java
+++ src/uet/oop/bomberman/entities/blocks/Bomb.java
@@ -7,130 +7,130 @@ import uet.oop.bomberman.graphics.Sprite;
 import static uet.oop.bomberman.BombermanGame.*;
 
 public class Bomb extends Entity {
-    private static final long bombLastTime = 2200;
-    private static final long uiLastTime = 160;
-    private int rx;
-    private int ry;
-    private long bombStartTime;
-    public boolean isExploded;
-    private int animationTransform = 1;
-    private int countTransform = 0;
-    public boolean ui;
+  private static final long bombLastTime = 2200;
+  private static final long uiLastTime = 480;
+  private int rx;
+  private int ry;
+  private long bombStartTime;
+  public boolean isExploded;
+  private int animationTransform = 1;
+  private int countTransform = 0;
+  public boolean ui;
 
-    private static Bomb left;
-    private static Bomb right;
-    private static Bomb up;
-    private static Bomb down;
+  private static Bomb left;
+  private static Bomb right;
+  private static Bomb up;
+  private static Bomb down;
 
-    public Bomb(int rx, int ry, Image img, boolean isExploded, boolean ui) {
-        super(rx, ry, img);
-        this.rx = rx;
-        this.ry = ry;
-        this.isExploded = isExploded;
-        this.ui = ui;
-        this.bombStartTime = System.currentTimeMillis();
-    }
+  public Bomb(int rx, int ry, Image img, boolean isExploded, boolean ui) {
+    super(rx, ry, img);
+    this.rx = rx;
+    this.ry = ry;
+    this.isExploded = isExploded;
+    this.ui = ui;
+    this.bombStartTime = System.currentTimeMillis();
+  }
 
-    public void explosion() {
-        if (countTransform % 10 == 0) {
-            if (animationTransform == 1) {
-                this.img = Sprite.bomb_exploded.getFxImage();
-                animationTransform = 2;
-                Bomb.left =
-                        new Bomb(rx - 1, ry, Sprite.explosion_horizontal_left_last.getFxImage(), false, true);
-                Bomb.right =
-                        new Bomb(rx + 1, ry, Sprite.explosion_horizontal_right_last.getFxImage(), false, true);
-                Bomb.up =
-                        new Bomb(rx, ry - 1, Sprite.explosion_vertical_top_last.getFxImage(), false, true);
-                Bomb.down =
-                        new Bomb(rx, ry + 1, Sprite.explosion_vertical_down_last.getFxImage(), false, true);
-                bombs.add(left);
-                bombs.add(right);
-                bombs.add(up);
-                bombs.add(down);
-            } else if (animationTransform == 2) {
-                this.img = Sprite.bomb_exploded1.getFxImage();
-                animationTransform = 3;
-                if (Bomb.left != null) {
-                    Bomb.left.img = Sprite.explosion_horizontal_left_last1.getFxImage();
-                }
-                if (Bomb.right != null) {
-                    Bomb.right.img = Sprite.explosion_horizontal_right_last1.getFxImage();
-                }
-                if (Bomb.up != null) {
-                    Bomb.up.img = Sprite.explosion_vertical_top_last1.getFxImage();
-                }
-                if (Bomb.down != null) {
-                    Bomb.down.img = Sprite.explosion_vertical_down_last1.getFxImage();
-                }
-            } else if (animationTransform == 3) {
-                this.img = Sprite.bomb_exploded2.getFxImage();
-                animationTransform = 4;
-                if (Bomb.left != null) {
-                    Bomb.left.img = Sprite.explosion_horizontal_left_last2.getFxImage();
-                }
-                if (Bomb.right != null) {
-                    Bomb.right.img = Sprite.explosion_horizontal_right_last2.getFxImage();
-                }
-                if (Bomb.up != null) {
-                    Bomb.up.img = Sprite.explosion_vertical_top_last2.getFxImage();
-                }
-                if (Bomb.down != null) {
-                    Bomb.down.img = Sprite.explosion_vertical_down_last2.getFxImage();
-                }
-            } else {
-                this.isExploded = true;
-                left.isExploded = true;
-                right.isExploded = true;
-                up.isExploded = true;
-                down.isExploded = true;
-            }
+  public void explosion() {
+    if (countTransform % 10 == 0) {
+      if (animationTransform == 1) {
+        this.img = Sprite.bomb_exploded.getFxImage();
+        animationTransform = 2;
+        Bomb.left =
+            new Bomb(rx - 1, ry, Sprite.explosion_horizontal_left_last.getFxImage(), false, true);
+        Bomb.right =
+            new Bomb(rx + 1, ry, Sprite.explosion_horizontal_right_last.getFxImage(), false, true);
+        Bomb.up =
+            new Bomb(rx, ry - 1, Sprite.explosion_vertical_top_last.getFxImage(), false, true);
+        Bomb.down =
+            new Bomb(rx, ry + 1, Sprite.explosion_vertical_down_last.getFxImage(), false, true);
+        bombs.add(left);
+        bombs.add(right);
+        bombs.add(up);
+        bombs.add(down);
+      } else if (animationTransform == 2) {
+        this.img = Sprite.bomb_exploded1.getFxImage();
+        animationTransform = 3;
+        if (Bomb.left != null) {
+          Bomb.left.img = Sprite.explosion_horizontal_left_last1.getFxImage();
         }
-    }
-
-    @Override
-    public void update() {
-        if (!ui) {
-            if (System.currentTimeMillis() - this.bombStartTime > bombLastTime) {
-                countTransform++;
-                explosion();
-            }
-        } else {
-            if (System.currentTimeMillis() - this.bombStartTime > uiLastTime) {
-                isExploded = true;
-            }
+        if (Bomb.right != null) {
+          Bomb.right.img = Sprite.explosion_horizontal_right_last1.getFxImage();
         }
+        if (Bomb.up != null) {
+          Bomb.up.img = Sprite.explosion_vertical_top_last1.getFxImage();
+        }
+        if (Bomb.down != null) {
+          Bomb.down.img = Sprite.explosion_vertical_down_last1.getFxImage();
+        }
+      } else if (animationTransform == 3) {
+        this.img = Sprite.bomb_exploded2.getFxImage();
+        animationTransform = 4;
+        if (Bomb.left != null) {
+          Bomb.left.img = Sprite.explosion_horizontal_left_last2.getFxImage();
+        }
+        if (Bomb.right != null) {
+          Bomb.right.img = Sprite.explosion_horizontal_right_last2.getFxImage();
+        }
+        if (Bomb.up != null) {
+          Bomb.up.img = Sprite.explosion_vertical_top_last2.getFxImage();
+        }
+        if (Bomb.down != null) {
+          Bomb.down.img = Sprite.explosion_vertical_down_last2.getFxImage();
+        }
+      } else {
+        this.isExploded = true;
+        left.isExploded = true;
+        right.isExploded = true;
+        up.isExploded = true;
+        down.isExploded = true;
+      }
     }
+  }
 
-    public int getRx() {
-        return rx;
+  @Override
+  public void update() {
+    if (!ui) {
+      if (System.currentTimeMillis() - this.bombStartTime > bombLastTime) {
+        countTransform++;
+        explosion();
+      }
+    } else {
+      if (System.currentTimeMillis() - this.bombStartTime > uiLastTime) {
+        isExploded = true;
+      }
     }
+  }
 
-    public void setRx(int rx) {
-        this.rx = rx;
-    }
+  public int getRx() {
+    return rx;
+  }
 
-    public int getRy() {
-        return ry;
-    }
+  public void setRx(int rx) {
+    this.rx = rx;
+  }
 
-    public void setRy(int ry) {
-        this.ry = ry;
-    }
+  public int getRy() {
+    return ry;
+  }
 
-    public long getBombStartTime() {
-        return bombStartTime;
-    }
+  public void setRy(int ry) {
+    this.ry = ry;
+  }
 
-    public void setBombStartTime(long bombStartTime) {
-        this.bombStartTime = bombStartTime;
-    }
+  public long getBombStartTime() {
+    return bombStartTime;
+  }
 
-    public boolean isExploded() {
-        return isExploded;
-    }
+  public void setBombStartTime(long bombStartTime) {
+    this.bombStartTime = bombStartTime;
+  }
 
-    public void setExploded(boolean exploded) {
-        isExploded = exploded;
-    }
+  public boolean isExploded() {
+    return isExploded;
+  }
+
+  public void setExploded(boolean exploded) {
+    isExploded = exploded;
+  }
 }
