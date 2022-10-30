844fb988e46f3cf8c218796d4374cb96f59a69bc
SamoySamoy <antalois23@gmail.com>
2022-10-30 11:20:27 +0700

    fix ui

--- src/uet/oop/bomberman/graphics/Sprite.java
+++ src/uet/oop/bomberman/graphics/Sprite.java
@@ -28,28 +28,30 @@ public class Sprite {
   public static Sprite wall = new Sprite(DEFAULT_SIZE, 5, 0, SpriteSheet.tiles, 16, 16);
   public static Sprite wall2 = new Sprite(DEFAULT_SIZE, 3, 2, SpriteSheet.tiles3, 16, 16);
   public static Sprite portal = new Sprite(DEFAULT_SIZE, 9, 1, SpriteSheet.tiles3, 14, 14);
-  /* 
+  /*
    * |-------------------------------------------------------------------------- | Bomber Sprites
    * |--------------------------------------------------------------------------
    */
-
   public static Sprite player_up = new Sprite(DEFAULT_SIZE, 0, 0, SpriteSheet.tiles, 12, 16);
+  public static Sprite player_down = new Sprite(DEFAULT_SIZE, 2, 0, SpriteSheet.tiles, 12, 15);
+  public static Sprite player_left = new Sprite(DEFAULT_SIZE, 3, 0, SpriteSheet.tiles, 10, 15);
+  public static Sprite player_right = new Sprite(DEFAULT_SIZE, 1, 0, SpriteSheet.tiles, 10, 16);
+
   public static Sprite player_up_1 = new Sprite(DEFAULT_SIZE, 0, 1, SpriteSheet.tiles, 12, 16);
   public static Sprite player_up_2 = new Sprite(DEFAULT_SIZE, 0, 2, SpriteSheet.tiles, 12, 15);
-  public static Sprite player_down = new Sprite(DEFAULT_SIZE, 2, 0, SpriteSheet.tiles, 12, 15);
+
   public static Sprite player_down_1 = new Sprite(DEFAULT_SIZE, 2, 1, SpriteSheet.tiles, 12, 15);
   public static Sprite player_down_2 = new Sprite(DEFAULT_SIZE, 2, 2, SpriteSheet.tiles, 12, 16);
-  public static Sprite player_left = new Sprite(DEFAULT_SIZE, 3, 0, SpriteSheet.tiles, 10, 15);
+
   public static Sprite player_left_1 = new Sprite(DEFAULT_SIZE, 3, 1, SpriteSheet.tiles, 11, 16);
   public static Sprite player_left_2 = new Sprite(DEFAULT_SIZE, 3, 2, SpriteSheet.tiles, 12, 16);
-  public static Sprite player_right = new Sprite(DEFAULT_SIZE, 1, 0, SpriteSheet.tiles, 10, 16);
+
   public static Sprite player_right_1 = new Sprite(DEFAULT_SIZE, 1, 1, SpriteSheet.tiles, 11, 16);
   public static Sprite player_right_2 = new Sprite(DEFAULT_SIZE, 1, 2, SpriteSheet.tiles, 12, 16);
 
-  public static Sprite player_dead = new Sprite(DEFAULT_SIZE, 4, 2, SpriteSheet.tiles, 14, 16);
-  public static Sprite player_dead_1 = new Sprite(DEFAULT_SIZE, 5, 2, SpriteSheet.tiles, 13, 15);
-  public static Sprite player_dead_2 = new Sprite(DEFAULT_SIZE, 6, 2, SpriteSheet.tiles, 16, 16);
-
+  public static Sprite player_dead1 = new Sprite(DEFAULT_SIZE, 4, 2, SpriteSheet.tiles, 14, 16);
+  public static Sprite player_dead2 = new Sprite(DEFAULT_SIZE, 5, 2, SpriteSheet.tiles, 13, 15);
+  public static Sprite player_dead3 = new Sprite(DEFAULT_SIZE, 6, 2, SpriteSheet.tiles, 16, 16);
   /*
    * |-------------------------------------------------------------------------- | Character
    * |--------------------------------------------------------------------------
--- src/uet/oop/bomberman/graphics/SpriteSheet.java
+++ src/uet/oop/bomberman/graphics/SpriteSheet.java
@@ -19,6 +19,8 @@ public class SpriteSheet {
   public static SpriteSheet tiles = new SpriteSheet("/textures/classic.png", 256);
   public static SpriteSheet tiles2 = new SpriteSheet("/textures/modern2.png", 272);
   public static SpriteSheet tiles3 = new SpriteSheet("/textures/modern3.png", 272);
+  public static SpriteSheet char1 = new SpriteSheet("/textures/char.png", 324);
+
   public SpriteSheet(String path, int size) {
     _path = path;
     SIZE = size;
