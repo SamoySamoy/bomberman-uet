25cdc5e2e324e4f79b048a6f56c8aa0cc68574a1
SamoySamoy <antalois23@gmail.com>
2022-11-01 17:10:55 +0700

    stage 2 setup

--- src/uet/oop/bomberman/LevelMap/LevelUp.java
+++ src/uet/oop/bomberman/LevelMap/LevelUp.java
@@ -3,38 +3,42 @@ package uet.oop.bomberman.LevelMap;
 import uet.oop.bomberman.entities.blocks.Bomb;
 
 import static uet.oop.bomberman.BombermanGame.*;
+import uet.oop.bomberman.BombermanGame;
 
 public class LevelUp {
-    public static void clearLevel() {
-        // clean and clear stage
-        isStopMoving = false;
-        isOver = false;
-        isLevelUp = false;
-        isPause = false;
-        Bomb.curBombLevel = 1;
-        entities.clear();
-        stillObjects.clear();
-        enemies.clear();
-        bombs.clear();
-        items.clear();
-        // clear bomb matrix, item matrix, objId,
-        for (int i = 0; i < LEVEL_WIDTH; i++) {
-            for (int j = 0; j < LEVEL_HEIGHT; j++) {
-                bombMatix[i][j] = 0;
-                itemMatrix[i][j] = 0;
-                objId[i][j] = 0;
-            }
-        }
-        bomberman.reset();
+  public static void clearLevel() {
+    // clean and clear stage
+    isStopMoving = false;
+    isOver = false;
+    isLevelUp = false;
+    isPause = false;
+    Bomb.curBombLevel = 1;
+    entities.clear();
+    stillObjects.clear();
+    enemies.clear();
+    bombs.clear();
+    items.clear();
+    // clear bomb matrix, item matrix, objId,
+    for (int i = 0; i < LEVEL_WIDTH; i++) {
+      for (int j = 0; j < LEVEL_HEIGHT; j++) {
+        bombMatix[i][j] = 0;
+        itemMatrix[i][j] = 0;
+        objId[i][j] = 0;
+      }
+    }
+    bomberman.reset();
+  }
+
+  public static void checkLevelUp() {
+    if (BombermanGame.level == 1) {
+      if (bomberman.getX() == portal1.getX() && bomberman.getY() == portal1.getY()) {
+        new Level2();
+      }
+    } else if (BombermanGame.level == 2) {
+      if (bomberman.getX() == portal2.getX() && bomberman.getY() == portal2.getY()) {
+        new Level3();
+      }
     }
 
-    public static void checkLevelUp() {
-        if (bomberman.getX() == portal1.getX() && bomberman.getY() == portal1.getY()) {
-            if (level == 1) {
-                new Level2();
-            } else if (level == 2) {
-                new Level3();
-            }
-        }
-    }
+  }
 }
--- src/uet/oop/bomberman/entities/blocks/Portal.java
+++ src/uet/oop/bomberman/entities/blocks/Portal.java
@@ -3,6 +3,7 @@ package uet.oop.bomberman.entities.blocks;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
 import static uet.oop.bomberman.BombermanGame.*;
+import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.LevelMap.LevelUp;
 
 public class Portal extends Entity {
@@ -12,9 +13,15 @@ public class Portal extends Entity {
 
   public static void checkPortal() {
     if (enemies.size() == 0 && !isLevelUp) {
-      stillObjects.add(portal1);
-      LevelUp.checkLevelUp();
-      System.out.println(level);
+      if (BombermanGame.level == 1) {
+        stillObjects.add(portal1);
+        LevelUp.checkLevelUp();
+        System.out.println(level);
+      } else if (BombermanGame.level == 2) {
+        stillObjects.add(portal2);
+        LevelUp.checkLevelUp();
+        System.out.println(level);
+      }
     }
   }
 
