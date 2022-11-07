817519022d2e5fe3b515d420bdca079a00e227d8
SamoySamoy <antalois23@gmail.com>
2022-11-07 14:08:22 +0700

    clean

--- src/uet/oop/bomberman/entities/bomberman/Bomber.java
+++ src/uet/oop/bomberman/entities/bomberman/Bomber.java
@@ -11,243 +11,243 @@ import uet.oop.bomberman.graphics.Sprite;
 import static uet.oop.bomberman.BombermanGame.*;
 
 public class Bomber extends MovableEntity {
-    // Animation when bomber dead
-    private int deadAnimation;
-    private int countDeadTransform;
-    // Bomb stats
-    private long lastPutBomb;
-    private int curBombRemain;
-    // Bomber speed stats
-    public static int BOMBER_WAIT_NEXT_STEP;
-    public static final int BOMBER_DEFAULT_SPEED = 8;
-    public static final int BOMBER_WAIT_NEXT_STEP_NORMAL = 4;
-    public static final int BOMBER_WAIT_NEXT_STEP_FAST = 2;
+  // Animation when bomber dead
+  private int deadAnimation;
+  private int countDeadTransform;
+  // Bomb stats
+  private long lastPutBomb;
+  private int curBombRemain;
+  // Bomber speed stats
+  public static int BOMBER_WAIT_NEXT_STEP;
+  public static final int BOMBER_DEFAULT_SPEED = 8;
+  public static final int BOMBER_WAIT_NEXT_STEP_NORMAL = 4;
+  public static final int BOMBER_WAIT_NEXT_STEP_FAST = 2;
 
-    public Bomber(int rx, int ry, Image img, boolean isAlive, String direction) {
-        super(rx, ry, img, isAlive, direction);
-        this.curBombRemain = 1;
-        this.deadAnimation = 1;
-        this.countDeadTransform = 0;
-        this.lastPutBomb = 0;
-        this.setNormalSpeedState();
-        // Set img
-        upImg[0] = Sprite.player_up.getFxImage();
-        upImg[1] = Sprite.player_up_1.getFxImage();
-        upImg[2] = Sprite.player_up_2.getFxImage();
+  public Bomber(int rx, int ry, Image img, boolean isAlive, String direction) {
+    super(rx, ry, img, isAlive, direction);
+    this.curBombRemain = 1;
+    this.deadAnimation = 1;
+    this.countDeadTransform = 0;
+    this.lastPutBomb = 0;
+    this.setNormalSpeedState();
+    // Set img
+    upImg[0] = Sprite.player_up.getFxImage();
+    upImg[1] = Sprite.player_up_1.getFxImage();
+    upImg[2] = Sprite.player_up_2.getFxImage();
 
-        downImg[0] = Sprite.player_down.getFxImage();
-        downImg[1] = Sprite.player_down_1.getFxImage();
-        downImg[2] = Sprite.player_down_2.getFxImage();
+    downImg[0] = Sprite.player_down.getFxImage();
+    downImg[1] = Sprite.player_down_1.getFxImage();
+    downImg[2] = Sprite.player_down_2.getFxImage();
 
-        leftImg[0] = Sprite.player_left.getFxImage();
-        leftImg[1] = Sprite.player_left_1.getFxImage();
-        leftImg[2] = Sprite.player_left_2.getFxImage();
+    leftImg[0] = Sprite.player_left.getFxImage();
+    leftImg[1] = Sprite.player_left_1.getFxImage();
+    leftImg[2] = Sprite.player_left_2.getFxImage();
 
-        rightImg[0] = Sprite.player_right.getFxImage();
-        rightImg[1] = Sprite.player_right_1.getFxImage();
-        rightImg[2] = Sprite.player_right_2.getFxImage();
+    rightImg[0] = Sprite.player_right.getFxImage();
+    rightImg[1] = Sprite.player_right_1.getFxImage();
+    rightImg[2] = Sprite.player_right_2.getFxImage();
+  }
+
+  @Override
+  public void move() {
+
+  }
+
+  @Override
+  protected void upStep() {
+    super.upStep();
+    this.checkPickItem();
+  }
+
+  @Override
+  protected void downStep() {
+    super.downStep();
+    this.checkPickItem();
+  }
+
+  @Override
+  protected void leftStep() {
+    super.leftStep();
+    this.checkPickItem();
+  }
+
+  @Override
+  protected void rightStep() {
+    super.rightStep();
+    this.checkPickItem();
+  }
+
+  // keyboard event
+  public void handleEventPress(KeyEvent e) {
+    switch (e.getCode()) {
+      case W:
+      case UP:
+        this.setMove("up");
+        break;
+      case S:
+      case DOWN:
+        this.setMove("down");
+        break;
+      case A:
+      case LEFT:
+        this.setMove("left");
+        break;
+      case D:
+      case RIGHT:
+        this.setMove("right");
+        break;
+      case SPACE:
+        this.putBomb();
+        break;
     }
+  }
 
-    @Override
-    public void move() {
-
+  // Check if bomber pick up item
+  private void checkPickItem() {
+    if (itemMatrix[this.rx][this.ry] != 0) {
+      // Set item effect
+      // Note: 4 5 6 7 8 are Speed, Flame, Bomb item, Bomb limit item, Time limit item respectively
+      switch (itemMatrix[this.rx][this.ry]) {
+        case 4: // High speed item
+          Bomber.BOMBER_WAIT_NEXT_STEP = Bomber.BOMBER_WAIT_NEXT_STEP_FAST;
+          break;
+        case 5: // Gain bomb power item
+          Bomb.gainBombLevel();
+          break;
+        case 6: // Gain amount of bomb can be put in once time
+          this.gainBombRemain();
+          break;
+        case 7: // Gain bomb limit
+          bomb_number += 10;
+          Menu.bomb.setText("Bomb " + bomb_number);
+          break;
+        case 8: // Gain time limit
+          time_number += 30;
+          Menu.time.setText("Time " + time_number);
+          break;
+      }
+      // Remove item
+      items.removeIf(item -> item.getRx() == this.rx && item.getRy() == this.ry);
+      objId[this.rx][this.ry] = 0;
+      itemMatrix[this.rx][this.ry] = 0;
     }
+  }
 
-    @Override
-    protected void upStep() {
-        super.upStep();
-        this.checkPickItem();
+  private void putBomb() {
+    if (curBombRemain > 0 && bomb_number > 0) {
+      // lower bomb can be put in the same period
+      this.lowerBombRemain();
+      // decrease bomb_number remain in bag
+      bomb_number--;
+      Menu.bomb.setText("Bomb " + bomb_number);
+      // Set new bomb
+      Bomb bomb = new Bomb(this.rx, this.ry, Sprite.bomb.getFxImage(), false, false);
+      new Sound("sound/put_bombs.wav", "putBomb");
+      bombs.add(bomb);
     }
+  }
 
-    @Override
-    protected void downStep() {
-        super.downStep();
-        this.checkPickItem();
-    }
+  private void setNormalSpeedState() {
+    this.speed = Bomber.BOMBER_DEFAULT_SPEED;
+    Bomber.BOMBER_WAIT_NEXT_STEP = Bomber.BOMBER_WAIT_NEXT_STEP_NORMAL;
+  }
 
-    @Override
-    protected void leftStep() {
-        super.leftStep();
-        this.checkPickItem();
-    }
-
-    @Override
-    protected void rightStep() {
-        super.rightStep();
-        this.checkPickItem();
-    }
-
-    // keyboard event
-    public void handleEventPress(KeyEvent e) {
-        switch (e.getCode()) {
-            case W:
-            case UP:
-                this.setMove("up");
-                break;
-            case S:
-            case DOWN:
-                this.setMove("down");
-                break;
-            case A:
-            case LEFT:
-                this.setMove("left");
-                break;
-            case D:
-            case RIGHT:
-                this.setMove("right");
-                break;
-            case SPACE:
-                this.putBomb();
-                break;
+  @Override
+  public void killedByBomb() {
+    for (Bomb bomb : bombs) {
+      if (bomb.isFinal()) {
+        if (bomb.getRx() == this.rx && bomb.getRy() == this.ry) {
+          this.killedByEnemy();
+          isOver = true;
         }
+      }
     }
+  }
 
-    // Check if bomber pick up item
-    private void checkPickItem() {
-        if (itemMatrix[this.rx][this.ry] != 0) {
-            // Set item effect
-            // Note: 4 5 6 7 8 are Speed, Flame, Bomb item, Bomb limit item, Time limit item respectively
-            switch (itemMatrix[this.rx][this.ry]) {
-                case 4: // High speed item
-                    Bomber.BOMBER_WAIT_NEXT_STEP = Bomber.BOMBER_WAIT_NEXT_STEP_FAST;
-                    break;
-                case 5: // Gain bomb power item
-                    Bomb.gainBombLevel();
-                    break;
-                case 6: // Gain amount of bomb can be put in once time
-                    this.gainBombRemain();
-                    break;
-                case 7: // Gain bomb limit
-                    bomb_number += 10;
-                    Menu.bomb.setText("Bomb " + bomb_number);
-                    break;
-                case 8: // Gain time limit
-                    time_number += 30;
-                    Menu.time.setText("Time " + time_number);
-                    break;
-            }
-            // Remove item
-            items.removeIf(item -> item.getRx() == this.rx && item.getRy() == this.ry);
-            objId[this.rx][this.ry] = 0;
-            itemMatrix[this.rx][this.ry] = 0;
+  public void killedByEnemy() {
+    isStopMoving = true;
+    isAlive = false;
+    if (!this.isAlive) {
+      // transform from dead1 state to dead3 state
+      if (countDeadTransform % 15 == 0) {
+        if (deadAnimation == 1) {
+          this.img = Sprite.player_dead.getFxImage();
+          deadAnimation = 2;
+        } else if (deadAnimation == 2) {
+          this.img = Sprite.player_dead_1.getFxImage();
+          deadAnimation = 3;
+        } else if (deadAnimation == 3) {
+          this.img = Sprite.player_dead_2.getFxImage();
+          deadAnimation = 4;
+        } else {
+          isOver = true;
         }
+      }
     }
+  }
 
-    private void putBomb() {
-        if (curBombRemain > 0 && bomb_number > 0) {
-            // lower bomb can be put in the same period
-            this.lowerBombRemain();
-            // decrease bomb_number remain in bag
-            bomb_number--;
-            Menu.bomb.setText("Bomb " + bomb_number);
-            // Set new bomb
-            Bomb bomb = new Bomb(this.rx, this.ry, Sprite.bomb.getFxImage(), false, false);
-            new Sound("sound/put_bombs.wav", "putBomb");
-            bombs.add(bomb);
-        }
-    }
+  // new level, new game, restart game bomber reset
+  public void reset() {
+    this.setAlive(true);
+    this.setDeadAnimation(1);
+    this.setCountDeadTransform(0);
+    this.setLastPutBomb(0);
+    this.setRx(1);
+    this.setRy(1);
+    this.setX(32);
+    this.setY(32);
+    this.setDirection("right");
+    this.setImage(Sprite.player_right.getFxImage());
+    this.setCurBombRemain(1);
+    this.setSteps(0);
+    this.setCountToRun(0);
+    this.setNormalSpeedState();
+    Bomb.curBombLevel = 1;
+  }
 
-    private void setNormalSpeedState() {
-        this.speed = Bomber.BOMBER_DEFAULT_SPEED;
-        Bomber.BOMBER_WAIT_NEXT_STEP = Bomber.BOMBER_WAIT_NEXT_STEP_NORMAL;
-    }
+  @Override
+  public void update() {
+    countDeadTransform++;
+    killedByBomb();
+  }
 
-    @Override
-    public void killedByBomb() {
-        for (Bomb bomb : bombs) {
-            if (bomb.isFinal()) {
-                if (bomb.getRx() == this.rx && bomb.getRy() == this.ry) {
-                    this.killedByEnemy();
-                    isOver = true;
-                }
-            }
-        }
-    }
+  public int getCurBombRemain() {
+    return curBombRemain;
+  }
 
-    public void killedByEnemy() {
-        isStopMoving = true;
-        isAlive = false;
-        if (!this.isAlive) {
-            // transform from dead1 state to dead3 state
-            if (countDeadTransform % 15 == 0) {
-                if (deadAnimation == 1) {
-                    this.img = Sprite.player_dead.getFxImage();
-                    deadAnimation = 2;
-                } else if (deadAnimation == 2) {
-                    this.img = Sprite.player_dead_1.getFxImage();
-                    deadAnimation = 3;
-                } else if (deadAnimation == 3) {
-                    this.img = Sprite.player_dead_2.getFxImage();
-                    deadAnimation = 4;
-                } else {
-                    isOver = true;
-                }
-            }
-        }
-    }
+  public void setCurBombRemain(int curBombRemain) {
+    this.curBombRemain = curBombRemain;
+  }
 
-    // new level, new game, restart game bomber reset
-    public void reset() {
-        this.setAlive(true);
-        this.setDeadAnimation(1);
-        this.setCountDeadTransform(0);
-        this.setLastPutBomb(0);
-        this.setRx(1);
-        this.setRy(1);
-        this.setX(32);
-        this.setY(32);
-        this.setDirection("right");
-        this.setImage(Sprite.player_right.getFxImage());
-        this.setCurBombRemain(1);
-        this.setSteps(0);
-        this.setCountToRun(0);
-        this.setNormalSpeedState();
-        Bomb.curBombLevel = 1;
-    }
+  public void gainBombRemain() {
+    curBombRemain++;
+  }
 
-    @Override
-    public void update() {
-        countDeadTransform++;
-        killedByBomb();
-    }
+  public void lowerBombRemain() {
+    curBombRemain--;
+  }
 
-    public int getCurBombRemain() {
-        return curBombRemain;
-    }
+  public int getDeadAnimation() {
+    return deadAnimation;
+  }
 
-    public void setCurBombRemain(int curBombRemain) {
-        this.curBombRemain = curBombRemain;
-    }
+  public void setDeadAnimation(int deadAnimation) {
+    this.deadAnimation = deadAnimation;
+  }
 
-    public void gainBombRemain() {
-        curBombRemain++;
-    }
+  public int getCountDeadTransform() {
+    return countDeadTransform;
+  }
 
-    public void lowerBombRemain() {
-        curBombRemain--;
-    }
+  public void setCountDeadTransform(int countDeadTransform) {
+    this.countDeadTransform = countDeadTransform;
+  }
 
-    public int getDeadAnimation() {
-        return deadAnimation;
-    }
+  public long getLastPutBomb() {
+    return lastPutBomb;
+  }
 
-    public void setDeadAnimation(int deadAnimation) {
-        this.deadAnimation = deadAnimation;
-    }
-
-    public int getCountDeadTransform() {
-        return countDeadTransform;
-    }
-
-    public void setCountDeadTransform(int countDeadTransform) {
-        this.countDeadTransform = countDeadTransform;
-    }
-
-    public long getLastPutBomb() {
-        return lastPutBomb;
-    }
-
-    public void setLastPutBomb(long lastPutBomb) {
-        this.lastPutBomb = lastPutBomb;
-    }
+  public void setLastPutBomb(long lastPutBomb) {
+    this.lastPutBomb = lastPutBomb;
+  }
 }
